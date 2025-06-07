6644cb65933e02d761d41ec1cacf2c273f5b1c1e
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2025-06-07 14:00:54 -0400

    added init files for Strapi x nextjs

---
+++ webserver/next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+
+  // Allow images from localhost
+  images: {
+    domains: ["localhost"],
+  },
+};
+
+
+export default nextConfig;
